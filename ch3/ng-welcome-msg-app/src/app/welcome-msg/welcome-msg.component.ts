11c09ccf7f8a14a9887e01b0cc5379ef8e6329da
elon.kwon <elon.kwon@kakaocorp.com>
2017-11-15 18:08:27 +0900

    welcome-msg 콤포넌트 구현

--- ch3/ng-welcome-msg-app/src/app/welcome-msg/welcome-msg.component.ts
+++ ch3/ng-welcome-msg-app/src/app/welcome-msg/welcome-msg.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { I18nSupportService } from '../i18n-support.service';
 
 @Component({
   selector: 'app-welcome-msg',
   templateUrl: './welcome-msg.component.html',
   styleUrls: ['./welcome-msg.component.css']
 })
-export class WelcomeMsgComponent implements OnInit {
+export class WelcomeMsgComponent implements AfterViewInit {
+  userName = "";
+  welcomeMsg: String;
+  private valid = false;
+  private static CHK_KEYUP_WAIT_SEC = 5000;
 
-  constructor() { }
+  constructor(public i18nSupporter: I18nSupportService) {
 
-  ngOnInit() {
   }
 
+  showWelcomeMsg() {
+  	this.welcomeMsg = this.i18nSupporter.getWelcomeMsg(this.userName);
+  }
+
+  ngAfterViewInit() {
+  	const checkTouchedFn = () => {
+  		if(this.valid) return;
+  		alert('이름을 입력해 주세요');
+  	};
+
+  	setTimeout(checkTouchedFn, WelcomeMsgComponent.CHK_KEYUP_WAIT_SEC);
+  }
+
+  onChange() {
+  	this.valid = this.userName.length > 0;
+  }
 }

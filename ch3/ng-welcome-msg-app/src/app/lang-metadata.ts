e15a5f74bb96974da92ad279fea5c66e23908e92
elon.kwon <elon.kwon@kakaocorp.com>
2017-11-15 18:56:35 +0900

    lang-metadata.ts (언어 메타데이터) 구현

---
+++ ch3/ng-welcome-msg-app/src/app/lang-metadata.ts
@@ -0,0 +1,17 @@
+export const LANG_METADATA = [
+	{
+		code: 'ko',
+		name: '한국어',
+		msg: '안녕하세요'
+	},
+	{
+		code: 'en',
+		name: '영어',
+		msg: 'Hello'
+	},
+	{
+		code: 'fr',
+		name: '불어',
+		msg: 'Bonjour'
+	}
+];

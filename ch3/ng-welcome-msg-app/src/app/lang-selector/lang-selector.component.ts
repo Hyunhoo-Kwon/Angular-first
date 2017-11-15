94cbc8c46bec720ebc06719c5064e5fd34ef7d7e
elon.kwon <elon.kwon@kakaocorp.com>
2017-11-15 18:10:23 +0900

    lang-selector 콤포넌트 구현

--- ch3/ng-welcome-msg-app/src/app/lang-selector/lang-selector.component.ts
+++ ch3/ng-welcome-msg-app/src/app/lang-selector/lang-selector.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { I18nSupportService } from '../i18n-support.service';
 
 @Component({
   selector: 'app-lang-selector',
   templateUrl: './lang-selector.component.html',
   styleUrls: ['./lang-selector.component.css']
 })
-export class LangSelectorComponent implements OnInit {
+export class LangSelectorComponent {
+  langCode: String;
 
-  constructor() { }
+  constructor(public i18nSupporter: I18nSupportService) {
+  	this.langCode = i18nSupporter.langCode;
+  }
 
-  ngOnInit() {
+  setLangCode(langCode) {
+  	this.langCode = langCode;
+  	this.i18nSupporter.langCode = langCode;
   }
 
 }

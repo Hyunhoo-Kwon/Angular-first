4f13c63dd55abbe0d5b1095ee1370d21341e4642
elon.kwon <elon.kwon@kakaocorp.com>
2017-11-16 11:48:30 +0900

    머티리얼 패키지 추가

--- ch3/ng-welcome-msg-app/src/app/app.module.ts
+++ ch3/ng-welcome-msg-app/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, COMPOSITION_BUFFER_MODE } from '@angular/forms';
-import { I18nSupportService } from './i18n-support.service';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatSnackBarModule, MatCardModule, MatInputModule, MatRadioModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 import { LangSelectorComponent } from './lang-selector/lang-selector.component';
 import { LangSelectorBtnPipe } from './lang-selector/lang-selector-btn.pipe';
+import { I18nSupportService } from './i18n-support.service';
 
 
 @NgModule({
@@ -19,7 +20,9 @@ import { LangSelectorBtnPipe } from './lang-selector/lang-selector-btn.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule, 
+    MatToolbarModule, MatSnackBarModule, MatCardModule, MatInputModule, MatRadioModule, MatButtonModule
   ],
   providers: [{provide: COMPOSITION_BUFFER_MODE, useValue: false}, I18nSupportService],
   bootstrap: [AppComponent]

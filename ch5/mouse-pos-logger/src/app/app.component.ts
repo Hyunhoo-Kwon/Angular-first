4b5a6c53908854abfddbac6bbbc33c7bcfa83d05
elon.kwon <elon.kwon@kakaocorp.com>
2017-12-12 17:22:46 +0900

    app.component: 로그 출력 기능 구현

--- ch5/mouse-pos-logger/src/app/app.component.ts
+++ ch5/mouse-pos-logger/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MySpecialLoggerService } from './my-special-logger.service';
+import { LogLevel } from './log-level.enum';
 
 @Component({
   selector: 'mpl-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mpl';
+  title = 'mpl works!';
+  logger: MySpecialLoggerService;
+
+  constructor() {
+  	this.logger = new MySpecialLoggerService(LogLevel.INFO);
+  	this.testLoggerLevel();
+  }
+
+  testLoggerLevel() {
+  	console.log("== Default(Info) Log Level ==");
+  	this.logger.debug("test logging... in debug");
+  	this.logger.info("test logging... in info");
+  	this.logger.warn("test logging... in warn");
+  	this.logger.error("test logging... in error");
+
+  	this.logger.logLevel = LogLevel.DEBUG;
+  	console.log("== Debug Log Level ==");
+  	this.logger.debug("test logging... in debug");
+  	this.logger.info("test logging... in info");
+  	this.logger.warn("test logging... in warn");
+  	this.logger.error("test logging... in error");
+
+	this.logger.logLevel = LogLevel.WARN;
+  	console.log("== Warn Log Level ==");
+  	this.logger.debug("test logging... in debug");
+  	this.logger.info("test logging... in info");
+  	this.logger.warn("test logging... in warn");
+  	this.logger.error("test logging... in error");
+
+  	this.logger.logLevel = LogLevel.ERROR;
+  	console.log("== Error Log Level ==");
+  	this.logger.debug("test logging... in debug");
+  	this.logger.info("test logging... in info");
+  	this.logger.warn("test logging... in warn");
+  	this.logger.error("test logging... in error");
+  }
 }
